ed2398aa7242a15d7249c0a928ce26187ae50745
Vlad Orlenko <vlad@orlenko.ca>
2023-10-24 15:36:51 -0400

    Making it better

--- robota/command.py
+++ robota/command.py
@@ -1,9 +1,28 @@
-import subprocess
+from collections import defaultdict
+from functools import wraps
 from traceback import print_exc
+
+from rich.console import Console
 
 from .errors import RobotaError
 from .github import commit_all_and_push, create_pull, get_current_branch
 from .jira import add_comment_to_issue, get_issue, set_issue_status
+
+console = Console()
+
+commands = {}
+
+def make_command(*args):
+    def decorator(func):
+        for arg in args:
+            commands[arg] = func
+
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            return func(*args, **kwargs)
+
+        return wrapper
+    return decorator
 
 
 def command(*args):
@@ -11,10 +30,9 @@ def command(*args):
     print("Running command with args:", *args)
     cmd = args[0]
     cmdargs = args[1:]
-    handlers = {"pr": make_pr}
     default_handler = lambda *a: unknown_command(cmd, *a)
     try:
-        return handlers.get(cmd, default_handler)(*cmdargs)
+        return commands.get(cmd, default_handler)(*cmdargs)
     except RobotaError as rerr:
         print(f"Exiting because of {rerr}")
         return 42
@@ -27,30 +45,13 @@ def command(*args):
 def unknown_command(*args):
     """Handle unknown / unexpected commands."""
     print("Error: Unexpected command:", " ".join(args))
+    help()
     return 1
 
 
+@make_command('pr', 'PR', 'pull')
 def make_pr(commit_message=None, ready_for_review=False):
-    """Commit and push current directory, create a draft PR or PR from it, add a Jira comment, and mark the Jira story as "in progress" or "in review", depending on arguments.
-
-    Assume that the branch name is in the format vlad-{jira_story_id}-{slugified-title}.
-
-    Based on the JIRA story ID, get the actual title of the story from Jira API (using get_issue from .jira module)/
-
-    Commit current code as follows:
-    > git add . && git commit -am "[{jira-story-id}] {jira-story-title}" && git push
-
-    If a commit_message argument is provided, it is appended as a second line in the commit message:
-    > git add . && git commit -am "[{jira-story-id}] {jira-story-title}\n{commit_message}" && git push
-
-    After committing and pushing, create a draft PR or PR based on this branch (depending on ready_for_review value), using Github API. The PR description should say:
-    "This PR is related to Jira story: {jira-story-link}."
-
-    After the PR is created, copy its URL and add a comment to the Jira story:
-    "PR: {pr_url_link}"
-
-    Then, set the Jira story status to "in progress" or "in review", depending on ready_for_review value.
-    """
+    """Commit and push current directory, create a draft PR or PR from it, add a Jira comment, and mark the Jira story as "In Progress" or "In Review", depending on arguments."""
     current_branch = get_current_branch()
     jira_story_id = current_branch.split("-")[1]
     jira_story = get_issue(jira_story_id)
@@ -66,3 +67,14 @@ def make_pr(commit_message=None, ready_for_review=False):
     add_comment_to_issue(jira_story_id, jira_comment)
     status = "In Review" if ready_for_review else "In Progress"
     set_issue_status(jira_story_id, status)
+
+
+@make_command('help', 'h', '?', '-?', '-h')
+def help():
+    """Print this help screen"""
+    console.print("Available commands:")
+    grouped_evaluators = defaultdict(list)
+    for name, fn in commands.items():
+        grouped_evaluators[fn].append(name)
+    for fn, names in grouped_evaluators.items():
+        console.print(f"  {', '.join(names)}: {commands[names[0]].__doc__}")
