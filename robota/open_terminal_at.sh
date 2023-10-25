828e81b58c203de6d0c616d3e3c626a0509a8777
Vlad Orlenko <vlad@orlenko.ca>
2023-10-25 11:10:33 -0400

    Open terminal command

---
+++ robota/open_terminal_at.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Check if a directory argument was provided
+if [[ -z "$1" ]]; then
+    echo "Please provide a directory."
+    exit 1
+fi
+
+# Convert the directory argument to an absolute path
+dir=$(cd "$1"; pwd)
+
+# Determine the OS and open a new terminal window at the specified directory
+case "$(uname)" in
+    "Linux")
+        # For Ubuntu, using Terminator
+        terminator --working-directory="$dir" &>/dev/null
+        ;;
+    "Darwin")
+        # For macOS, using iTerm2
+        osascript <<EOF
+tell application "iTerm"
+    create window with default profile
+    tell current session of current window
+        write text "cd $dir && clear"
+    end tell
+end tell
+EOF
+        ;;
+    *)
+        echo "Unsupported OS."
+        exit 1
+        ;;
+esac
