a13fca61a69a6a0b1727d13e0e6f02550e8061b8
Vlad Orlenko <vlad@orlenko.ca>
2023-10-26 11:59:15 -0400

    Updating the script

--- robota/is_clean.sh
+++ robota/is_clean.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-# Check for uncommitted changes
+# Check for uncommitted changes in the working directory
 if ! git diff --quiet || ! git diff --cached --quiet; then
   echo "There are uncommitted changes."
   exit 1
+fi
+
+# Check for unpushed commits
+if git for-each-ref --format '%(upstream:short)' $(git symbolic-ref -q HEAD) | \
+   grep -q '.' && [ "$(git rev-list @{u}..)" ]; then
+  echo "There are unpushed commits."
+  exit 1
 else
-  echo "Working directory is clean."
+  echo "Working directory is clean and all commits are pushed."
   exit 0
 fi
